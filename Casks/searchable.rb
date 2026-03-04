031835e29ea669ea19c5ef90a30fd8be91a22d78
Myles Nosa <mylesnosa@gmail.com>
2026-03-04 02:14:46 -0800

    bump v0.3.0 — rename sessions

--- Casks/searchable.rb
+++ Casks/searchable.rb
@@ -1,6 +1,6 @@
 cask "searchable" do
-  version "0.2.2"
-  sha256 "8c756b91288f6df065baf7dfb7d3968029594ac625cf27f8689f58b066ae47dd"
+  version "0.3.0"
+  sha256 "ef6b26e15326c08774272beaabf9298e3a111ceab6ff1023477a4078c58087cc"
 
   url "https://github.com/mylesndavid/searchable/releases/download/v#{version}/Searchable_#{version}_aarch64.dmg"
   name "Searchable"
