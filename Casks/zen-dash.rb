3e1f4854cc9799634e309212d3d5e765bbfe422d
Myles Nosa <mylesnosa@gmail.com>
2026-03-09 12:16:14 -0700

    Update zen-dash SHA for latest build

--- Casks/zen-dash.rb
+++ Casks/zen-dash.rb
@@ -1,6 +1,6 @@
 cask "zen-dash" do
   version "1.0.0"
-  sha256 "cd62cf95f46ea541eb2f3d205755b8dde10d186ef22aaf10e2d4061a5ab735e7"
+  sha256 "e9c5c35f04a2ec8e4d0065a0c57279471abd6c0204107ef112eaf84d804af120"
 
   url "https://github.com/mylesndavid/zen-dash/releases/download/v#{version}/Zen.Dash-#{version}-arm64.dmg"
   name "Zen Dash"
